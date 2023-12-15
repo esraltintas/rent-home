b9dd4a6dcffa07c67194a794e3413f2090ca038f
Esra Aydemir <es.altintas@gmail.com>
2023-12-15 10:41:38 +0300

    Add successful page

---
+++ src/pages/rented.tsx
@@ -0,0 +1,12 @@
+const SuccessfulRentPage: React.FC = () => {
+  return (
+    <div className="flex flex-col justify-center content-center items-center mt-20 gap-6">
+      <h1 className="text-3xl font-bold">Successful</h1>
+      <p className="text-2xl font-medium">
+        Thank you for renting home on Buena
+      </p>
+    </div>
+  );
+};
+
+export default SuccessfulRentPage;
